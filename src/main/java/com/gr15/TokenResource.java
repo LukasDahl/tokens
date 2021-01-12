9b12c070eb74cd01071d8ada0e919442bb6cd652
Amtoft <lukas@amtoft.eu>
2021-01-12 15:59:41 +0100

    Basic post functionality

--- src/main/java/com/gr15/TokenResource.java
+++ src/main/java/com/gr15/TokenResource.java
@@ -1,16 +1,71 @@
 package com.gr15;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.security.SecureRandom;
 
 @Path("/tokens")
 public class TokenResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    private static final String SYMBOLS = "ABCDEFGJKLMNPRSTUVWXYZ0123456789";
+    private static final int TOKEN_LENGTH = 23;
+    private static final int TOKEN_WORD_LENGTH = 5;
+
+    HashMap<String, LinkedList<String>> tokenMap = new HashMap<>();
+    SecureRandom random = new SecureRandom();
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public JsonArray createTokens(JsonObject json) {
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+
+        if (!tokenMap.containsKey(json.getString("id"))) {
+            tokenMap.put(json.getString("id"), new LinkedList<String>());
+        }
+
+        if (tokenMap.get(json.getString("id")).size() <= 1) {
+
+            for (int i = 0; i < json.getInt("count"); i++) {
+
+                String token = tokenBuilder();
+                arrayBuilder.add(token);
+                tokenMap.get(json.getString("id")).add(token);
+
+            }
+        }
+        else {
+            JsonObject response = Json.createObjectBuilder()
+                    .add("errorMessage", "Account has more than 1 token").build();
+            throw new BadRequestException(Response.status(400).entity(response).type(MediaType.APPLICATION_JSON).build());
+        }
+
+        return arrayBuilder.build();
+    }
+
+
+
+    public String tokenBuilder() {
+        char[] values = new char[TOKEN_LENGTH];
+
+        for (int i = 0; i < TOKEN_LENGTH; i++) {
+
+            if ((i + 1) % (TOKEN_WORD_LENGTH + 1) == 0)
+                values[i] = '-';
+            else
+                values[i] = SYMBOLS.charAt(random.nextInt(SYMBOLS.length()));
+
+        }
+
+        String token = new String(values);
+        System.out.println(token);
+        return token;
     }
 }
