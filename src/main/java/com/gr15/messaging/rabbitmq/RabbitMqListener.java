50a239500105d483a7a7555cbb35669a49807a56
Amtoft <lukas@amtoft.eu>
2021-01-15 17:48:28 +0100

    Testing without mq

--- src/main/java/com/gr15/messaging/rabbitmq/RabbitMqListener.java
+++ src/main/java/com/gr15/messaging/rabbitmq/RabbitMqListener.java
@@ -27,7 +27,7 @@ public class RabbitMqListener {
     public void listen(String exchangeName, String queueType, String topic) throws Exception {
 
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("172.17.0.2");
+        factory.setHost("localhost");
         factory.setUsername("g15");
         factory.setPassword("g15");
 
--- src/main/java/com/gr15/messaging/rabbitmq/RabbitMqSender.java
+++ src/main/java/com/gr15/messaging/rabbitmq/RabbitMqSender.java
@@ -21,7 +21,7 @@ public class RabbitMqSender implements IEventSender {
     public void sendEvent(Event event, String exchangeName, String queueType, String topic) throws Exception {
 
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("172.17.0.2");
+        factory.setHost("localhost");
         factory.setUsername("g15");
         factory.setPassword("g15");
 
