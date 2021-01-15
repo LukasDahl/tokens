59f8b0d6b7433a3070588bf699fb6acb44abe380
Amtoft <lukas@amtoft.eu>
2021-01-15 14:39:19 +0100

    Trying to clean before tests

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@
 
 set -e
 
-mvn clean package
+mvn clean package -Dquarkus.package.type=uber-jar
 
 docker build -t tokens .
 docker run -d -p8080:8080 tokens
 
 sleep 2s
 
-mvn test
+mvn clean test
