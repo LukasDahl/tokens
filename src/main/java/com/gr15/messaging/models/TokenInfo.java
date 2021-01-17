5f84fddc9a741cbbe27b15ebdf7142c3758c4789
Amtoft <lukas@amtoft.eu>
2021-01-17 23:50:36 +0100

    TokenInfo object aligned with payment

--- src/main/java/com/gr15/messaging/models/TokenInfo.java
+++ src/main/java/com/gr15/messaging/models/TokenInfo.java
@@ -5,14 +5,39 @@ package com.gr15.messaging.models;
  */
 
 public class TokenInfo {
-    public String Token;
-    public Boolean IsUsed;
-    public String CustomerId;
+    private String token;
+    private Boolean isUsed;
+    private String customerId;
 
     public TokenInfo(String token, Boolean isUsed, String customerId) {
-        Token = token;
-        IsUsed = isUsed;
-        CustomerId = customerId;
+        this.token = token;
+        this.isUsed = isUsed;
+        this.customerId = customerId;
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+
+    public Boolean getIsUsed() {
+        return this.isUsed;
+    }
+
+    public String getCustomerId() {
+        return this.customerId;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setIsUsed(Boolean isUsed) {
+        this.isUsed = isUsed;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
     }
 }
+
 
